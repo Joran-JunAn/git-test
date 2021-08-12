b164ec46b062005f845581f70222f6872181f114
joran <joran@qq.com>
2021-08-12 20:57:39 +0800

    second commit

--- src/main/java/com/joran/git/GitTest.java
+++ src/main/java/com/joran/git/GitTest.java
@@ -3,6 +3,6 @@ package com.joran.git;
 public class GitTest {
     public static void main(String[] args) {
        System.out.println("hello,git1");
-
+        System.out.println("hello,git2");
     }
 }
